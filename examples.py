0a46313eccdf59311607d3f551a2bae1b522a7c9
Artem Kravchenko <kravchenko.artyom1988@gmail.com>
2019-12-13 12:22:48 +0200

    Create examples.py

---
+++ examples.py
@@ -0,0 +1,8 @@
+n=int(input("enter the number of elements:"))
+a=[]
+for i in range(0,n):
+    elem=int(input("enter the element:"))
+    a.append(elem)
+avg=sum(a)/n
+print("average number is:",round(avg,2))
+
