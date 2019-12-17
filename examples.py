1770552ef049008a4b064d0aea6e32ffcb55e7bb
Artem Kravchenko <kravchenko.artyom1988@gmail.com>
2019-12-17 17:43:32 +0200

    Update examples.py

--- examples.py
+++ examples.py
@@ -172,3 +172,8 @@ for i in range(1,n+1):
         else:
             print('0', sep='',end=' ')
     print()
+
+#20)The program takes a number n and prints an inverted star pattern of the desired size:
+n=int(input("enter a number:"))
+for i in range(n,0,-1):
+    print((n-i)*' '+i*'*')
