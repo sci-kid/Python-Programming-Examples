6eb44ee1b6b512661603f10f306890d8c6237ed7
Artem Kravchenko <kravchenko.artyom1988@gmail.com>
2019-12-18 12:53:46 +0200

    Update examples.py

--- examples.py
+++ examples.py
@@ -185,3 +185,34 @@ while sieve:
     prime=min(sieve)
     sieve-=set(range(prime,n+1,prime))
     print(prime,end='\t')
+
+#22)The program takes in a date and checks if it a valid date and prints the incremented date if it is:
+date=input('enter a date:')
+dd,mm,yy=date.split('/')
+dd=int(dd)
+mm=int(mm)
+yy=int(yy)
+if(mm==1 or mm==3 or mm==5 or mm==7 or mm==8 or mm==10 or mm==12):
+    max1=31
+elif(mm==4 or mm==6 or mm==9 or mm==11):
+    max1=30
+elif(yy%4==0 and yy%100!=0 or yy%400==0):
+    max1=29
+else:
+    max1=28
+if (mm<1 or mm>12):
+    print('the date is invalid')
+elif (dd<1 or dd>max1):
+    print('the date is invalid')
+elif (dd==max1 and mm!=12):
+    dd=1
+    mm=mm+1
+    print('the incremented date is:',dd,mm,yy,sep='/')
+elif (dd==31 and mm==12):
+    dd=1
+    mm=1
+    yy=yy+1
+    print('the incremented date is:',dd,mm,yy,sep='/')
+else:
+    dd=dd+1
+    print('the incremented date is:', dd, mm, yy,sep='/')
