205064a0559773004efcfefdc295a59cd37cb921
Artem Kravchenko <kravchenko.artyom1988@gmail.com>
2019-12-20 17:16:56 +0200

    Update examples.py

--- examples.py
+++ examples.py
@@ -223,3 +223,10 @@ yy=int(input('enter years:'))
 rate=float(input('rate:'))
 interest=(principle*yy*rate)/100
 print('interest is:',interest)
+
+#24)The program takes in a year and checks whether it is a leap year or not:
+year=int(input('enter a year:'))
+if (year%4==0 and year%100!=0 or year%400==0):
+    print('leap year')
+else:
+    print('not leap year')
