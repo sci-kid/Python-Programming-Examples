2d376aa1b260ac99a81d76bf52f7ad506cf248f4
Artem Kravchenko <kravchenko.artyom1988@gmail.com>
2020-02-01 11:19:34 +0200

    Update examples.py

--- examples.py
+++ examples.py
@@ -730,6 +730,38 @@ def add_length(str_):
     return a
 
 
-"""64)
+"""64)The first input array contains the correct answers to an exam, like ["a", "a", "b", "d"]. 
+The second one is "answers" array and contains student's answers.
+
+The two arrays are not empty and are the same length. Return the score for this array of answers,
+giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer(empty string).
+
+If the score < 0, return 0.
+
+For example:
+
+checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
+checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
+
+"""
+def check_exam(arr1,arr2):
+    s=0
+    for i in range(len(arr1)):
+        if arr1[i]==arr2[i]:
+            s+=4
+        elif arr1[i]!=arr2[i]:
+            if arr2[i]=="":
+                s+=0
+            else:
+                s-=1
+    if s<0:
+        return 0
+    else:
+        return s
+
+
+"""65)
 
 """
